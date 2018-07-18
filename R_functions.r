fb5c5a1aa7172f534baea96d25f1e494ed5d1a42
okushu5 <okushu@gmail.com>
2018-07-17 20:56:25 -0400

    test

--- R_functions.r
+++ R_functions.r
@@ -1,6 +1,14 @@
+# Delete everything
+rm(list=ls())
+
+
 # glm_object, gam_obj, etc.  get the name fo the response variable.
 all.vars(object$terms)[attr(object$terms,"response")]
 
 all.vars(object$terms)[-attr(object$terms,"response")]
 
-test <- rnorm(100)
+# One way to measure importance/contribution of variables in a model.
+contribution = function(GLM_or_GAM_obj){
+o <- apply(predict(GLM_or_GAM_obj, type='terms'),2,sd)
+return(o/sum(o))
+}
